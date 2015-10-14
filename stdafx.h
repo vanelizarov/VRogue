7d08fc80136126868a56fb628b6ad24748f4bd08
vanelizarov <elizarov.vanya@gmail.com>
2015-10-14 20:00:36 +0300

    Added new level, features, fixed some bugs

---
+++ stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO: reference additional headers your program requires here
+
+#include "Utilities.h"
+#include "Strings.h"
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <Windows.h>
